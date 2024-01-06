063417cbe1e63865755642e289688b964479733f
Y-T10 <t06t06ca2@gmail.com>
2024-01-06 21:42:38 +0900

    Nuklearのヘッダファイルを追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "fmt/core.h"
 #include "fmt/printf.h"
 
+#define NK_IMPLEMENTATION
+#include "nuklear.h"
+
 int main(int argc, char* argv[]) {
     fmt::println("sample program with fmt!");
     return 0;
