73667711beb59ef34b61f8c01d0371e18cb33632
Y-T10 <t06t06ca2@gmail.com>
2024-01-09 13:01:36 +0900

    UI配置エリアクラスを追加

---
+++ include/AtmWidgetArea.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <cstddef>
+#include <type_traits>
+#include <variant>
+#include <cstdint>
+#include <vector>
+
+template<class position_type = int32_t, class length_type = std::size_t, class ...wigets>
+struct WidgetArea {
+    using widget_type = std::variant<wigets...>;
+    using widget_container = std::vector<widget_type>;
+
+    using iterator = typename widget_container::iterator;
+    using const_iterator = typename widget_container::const_iterator;
+
+    using size_type = std::tuple<length_type, length_type>;
+    using pos_type = std::tuple<length_type, length_type>;
+
+    widget_container widgets;
+    size_type size;
+    pos_type pos;
+
+    template<class T>
+    void push_back(const T& w) noexcept {
+        widgets.push_back(w);
+    };
+
+    const size_type area_size() noexcept {
+        return size;
+    }
+
+    const pos_type area_position() noexcept {
+        return pos;
+    }
+
+    template<class Func>
+    void resize(const size_type& s, const Func& func = Func{}) noexcept {
+        static_assert(std::is_invocable_v<Func, WidgetArea&, const size_type&>);
+        resize(s);
+        func(*this, size);
+    }
+    void resize(const size_type& s) noexcept {
+        size = s;
+    }
+    
+    void place(const pos_type& p) noexcept {
+        pos = p;
+    }
+
+    iterator begin() noexcept { return widgets.begin(); }
+    const_iterator begin() const noexcept { return widgets.begin(); }
+    iterator end() noexcept { return widgets.end(); }
+    const_iterator end() const noexcept { return widgets.end(); }
+};
