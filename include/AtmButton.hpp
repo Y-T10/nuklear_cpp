2e78e30ac1c1011f6458cc297f513c169e65db4b
Y-T10 <t06t06ca2@gmail.com>
2024-01-15 01:36:25 +0900

    Atmのボタンクラスを追加

---
+++ include/AtmButton.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include "AtmWidgetConcept.hpp"
+#include "AtmTypes.hpp"
+
+struct Button {
+    std::string text;
+    boundary_t button_area;
+    bool is_pressed;
+
+    const boundary_t boundary_area() noexcept {
+        return button_area;
+    }
+
+    void boundary_area(const boundary_t& area) noexcept {
+        button_area = area;
+    }
+
+    const bool pressed() {
+        return is_pressed;
+    }
+
+    void push() {
+        is_pressed = true;
+    }
+
+    void release() {
+        is_pressed = false;
+    }
+};
