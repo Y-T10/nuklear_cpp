5f88b56da1492aea6ff8f37c90ffdd12e3d74c9d
Y-T10 <t06t06ca2@gmail.com>
2024-01-15 01:43:19 +0900

    Atmのサンプルコードを追加

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,8 +1,24 @@
 #include "fmt/core.h"
 
-#include "nuklear-cpp.hpp"
+#include "AtmWidgetArea.hpp"
+#include "AtmButton.hpp"
+#include "AtmTypes.hpp"
 
 int main(int argc, char* argv[]) {
     fmt::println("sample program with fmt!");
+
+    WidgetArea2<Button> sampleArea{
+        .widgets = {},
+        .area_boundary = boundary_t{point_t{0,0},point_t{800,600}}
+    };
+
+    sampleArea.push_back(
+        Button{
+            .text = "hello",
+            .button_area = boundary_t{point_t{10, 10}, point_t{110, 30}},
+            .is_pressed = false
+        }
+    );
+
     return 0;
 }
