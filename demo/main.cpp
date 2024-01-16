3f256bb840abb5b9ee05f4643e9d819a5f14925b
Y-T10 <t06t06ca2@gmail.com>
2024-01-16 14:13:00 +0900

    SDL2_ttfのラッパー型と関数を追加

--- demo/main.cpp
+++ demo/main.cpp
@@ -67,6 +67,29 @@ namespace SDL {
     using Surface = SDL_ptr<SDL_Surface, SDL_FreeSurface>;
 }
 
+namespace SDL::TTF {
+    using Font = std::unique_ptr<TTF_Font, decltype([](TTF_Font* ptr){TTF_CloseFont(ptr);})>;
+
+    const Font OpenFont(const std::string& file, int point_size = 12) noexcept {
+        return Font(TTF_OpenFont(file.c_str(), point_size));
+    }
+
+    const std::optional<std::pair<int, int>> UTF8TextSize(const Font& font, const std::string text) noexcept {
+        if(!font.get()) {
+            return std::nullopt;
+        }
+        int w = 0, h = 0;
+        if(TTF_SizeUTF8(font.get(), text.c_str(), &w, &h) == 0) {
+            return std::make_optional(std::make_pair(w, h));
+        }
+        return std::nullopt;
+    }
+
+    const Surface WriteText(const Font& font, const std::string& text, const SDL_Color& fg, const SDL_Color& bg) noexcept {
+        return Surface(TTF_RenderUTF8_LCD(font.get(), text.c_str(), fg, bg));
+    }
+}
+
 int main(int argc, char* argv[]) {
     fmt::println("sample program with fmt!");
 
