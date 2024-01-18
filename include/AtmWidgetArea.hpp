20cfb840020e4318aab4ad8391093cd509943ff8
Y-T10 <t06t06ca2@gmail.com>
2024-01-18 22:46:07 +0900

    不要な記述とファイルを削除

--- include/AtmWidgetArea.hpp
+++ include/AtmWidgetArea.hpp
@@ -10,65 +10,6 @@
 
 #include "AtmWidgetConcept.hpp"
 
-template<class position_type = int32_t, class length_type = std::size_t, Widget<position_type, length_type> ...wigets>
-struct WidgetArea {
-    using widget_type = std::variant<wigets...>;
-    using widget_container = std::vector<widget_type>;
-
-    using iterator = typename widget_container::iterator;
-    using const_iterator = typename widget_container::const_iterator;
-
-    using size_type = std::tuple<length_type, length_type>;
-    using pos_type = std::tuple<position_type, position_type>;
-
-    widget_container widgets;
-    size_type size;
-    pos_type pos;
-
-    template<class T>
-    void push_back(const T& w) noexcept {
-        widgets.push_back(w);
-    };
-
-    const size_type area_size() noexcept {
-        return size;
-    }
-
-    const pos_type area_position() noexcept {
-        return pos;
-    }
-
-    template<class Func>
-    void resize_area(const size_type& s, const Func& func = Func{}) noexcept {
-        static_assert(std::is_invocable_v<Func, WidgetArea&, const size_type&>);
-        resize(s);
-        func(*this, size);
-    }
-    void resize_area(const size_type& s) noexcept {
-        size = s;
-    }
-    
-    void place(const pos_type& p) noexcept {
-        pos = p;
-    }
-
-    const bool under(const pos_type& p) noexcept {
-        const auto [w, h] = size;
-        const auto [x, y] = pos;
-        const auto [p_x, p_y] = p;
-        return
-            x <= p_x && p_x <= (x + w) &&
-            y <= p_y && p_y <= (y + h);
-    }
-
-    iterator begin() noexcept { return widgets.begin(); }
-    const_iterator begin() const noexcept { return widgets.begin(); }
-    iterator end() noexcept { return widgets.end(); }
-    const_iterator end() const noexcept { return widgets.end(); }
-};
-
-static_assert(Widget<int32_t, std::size_t, WidgetArea<>>);
-
 #include "boost/geometry/algorithms/disjoint.hpp"
 #include "AtmTypes.hpp"
 
--- include/AtmWidgetConcept.hpp
+++ include/AtmWidgetConcept.hpp
@@ -6,22 +6,6 @@
 
 #include "AtmTypes.hpp"
 
-/**
- * @brief UIクラスのコンセプト
- * @tparam position_type 位置座標の型
- * @tparam length_type 長さの型
- * @tparam T 検査する型
- */
-template<class position_type, class length_type, class T>
-concept Widget = requires(T& w) {
-    { w.area_size() } -> std::same_as<const std::tuple<length_type, length_type>>;
-    { w.area_position() } -> std::same_as<const std::tuple<position_type, position_type>>;
-    w.resize_area(std::tuple<length_type, length_type>{});
-    w.place(std::tuple<position_type, position_type>{});
-
-    { w.under(std::tuple<position_type, position_type>{}) } -> std::same_as<bool>;
-};
-
 template<class T, class coord_type>
 concept Widget2 = requires(T& w) {
     // 境界を返すbundary_area関数を要求する
