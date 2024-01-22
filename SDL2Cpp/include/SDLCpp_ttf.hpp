cf6abb5f1c001d975484bd9d0a5c066adbe99bbb
Y-T10 <t06t06ca2@gmail.com>
2024-01-22 23:07:27 +0900

    SDLCpp_ttfをファイル分割した

--- SDL2Cpp/include/SDLCpp_ttf.hpp
+++ SDL2Cpp/include/SDLCpp_ttf.hpp
@@ -8,24 +8,11 @@
 #include "SDL_ttf.h"
 
 namespace SDL2Cpp::TTF {
-    using Font = std::unique_ptr<TTF_Font, decltype([](TTF_Font* ptr){TTF_CloseFont(ptr);})>;
+    using Font = std::unique_ptr<TTF_Font, SDL2Cpp::SDL_deleter<TTF_Font, TTF_CloseFont>>;
 
-    inline const Font OpenFont(const std::string& file, int point_size = 12) noexcept {
-        return Font(TTF_OpenFont(file.c_str(), point_size));
-    }
+    const Font OpenFont(const std::string& file, int point_size = 12) noexcept;
 
-    inline const std::optional<std::pair<int, int>> UTF8TextSize(const Font& font, const std::string text) noexcept {
-        if(!font.get()) {
-            return std::nullopt;
-        }
-        int w = 0, h = 0;
-        if(TTF_SizeUTF8(font.get(), text.c_str(), &w, &h) == 0) {
-            return std::make_optional(std::make_pair(w, h));
-        }
-        return std::nullopt;
-    }
+    const std::optional<std::pair<int, int>> UTF8TextSize(const Font& font, const std::string text) noexcept;
 
-    inline const SDL2Cpp::Surface WriteText(const Font& font, const std::string& text, const SDL_Color& fg, const SDL_Color& bg) noexcept {
-        return SDL2Cpp::Surface(TTF_RenderUTF8_LCD(font.get(), text.c_str(), fg, bg));
-    }
+    const SDL2Cpp::Surface WriteText(const Font& font, const std::string& text, const SDL_Color& fg, const SDL_Color& bg) noexcept;
 }
