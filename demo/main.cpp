c0f2c82a6d2dfd3bd9c33a3e3e12b727ea4818e3
Y-T10 <t06t06ca2@gmail.com>
2024-01-24 01:27:36 +0900

    不要なインクルードを削除

--- demo/main.cpp
+++ demo/main.cpp
@@ -13,17 +13,6 @@
 
 #include "Fcpp.hpp"
 
-#include "SDL2/SDL.h"
-#include "SDL2/SDL_video.h"
-#include "SDL2/SDL_render.h"
-#include "SDL2/SDL_timer.h"
-#include "SDL2/SDL_scancode.h"
-#include "SDL_events.h"
-#include "SDL2/SDL_surface.h"
-#include "SDL2/SDL_pixels.h"
-#include "SDL_ttf.h"
-#include "SDL_rect.h"
-
 #include "AtmWidgetArea.hpp"
 #include "AtmButton.hpp"
 #include "AtmTypes.hpp"
